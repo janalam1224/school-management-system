4f45a804361363f2bac717e9a41e1eb6a57e2852
janalam <janalam1224@gmail.com>
2025-09-18 14:22:02 +0500

    Modify app.js

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import prisma from './config/db_config.js';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to school-management-system");
+const userObject = {
+  name: "janalam",
+  email:"janalam1224@gmail.com",
+  role: "principal",
+  password: "123456",
+}
+app.post("/api/users", async(req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { email:userObject.email }
+  });
+  if(user){
+    return res.status(409).json({ message: "User already exists" });
+  }
+  const newUser = await prisma.user.create({
+    data:userObject,
+  });
+  console.log(newUser);
+  return res.status(201).json({ message: "User created successfully", newUser });
 });
 
 const PORT = 3100;
