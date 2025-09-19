f72270051a8ece09d06e666c4453ca911b2ef8e2
janalam <janalam1224@gmail.com>
2025-09-19 22:03:02 +0500

    Modify app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import prisma from './config/db_config.js';
 import authRouter from './routes/authRouter.js';
+import principalRouter from './routes/principalRouter.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/auth", authRouter);
+app.use("/api/principal", principalRouter);
 
 const PORT = 3100;
 
