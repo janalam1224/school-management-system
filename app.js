e6c8a7fa9e7005a9916ae789b581ae02b8f06781
janalam <janalam1224@gmail.com>
2025-09-19 15:14:45 +0500

    Modify app.js

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 import express from 'express';
 import prisma from './config/db_config.js';
+import authRouter from './routes/authRouter.js';
 
 const app = express();
 
-const userObject = {
-  name: "janalam",
-  email:"janalam1224@gmail.com",
-  role: "principal",
-  password: "123456",
-}
-app.post("/api/users", async(req, res) => {
-  const user = await prisma.user.findUnique({
-    where: { email:userObject.email }
-  });
-  if(user){
-    return res.status(409).json({ message: "User already exists" });
-  }
-  const newUser = await prisma.user.create({
-    data:userObject,
-  });
-  console.log(newUser);
-  return res.status(201).json({ message: "User created successfully", newUser });
-});
+app.use(express.json());
+
+app.use("/auth", authRouter);
 
 const PORT = 3100;
 
