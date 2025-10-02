7d4a8b28e125bec78e9aaafb891b6167a8ad47f1
janalam <janalam1224@gmail.com>
2025-10-02 15:31:43 +0500

    Modify authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import prisma from "../config/db_config.js";
 import bcrypt from 'bcrypt';
+import { generateToken } from "../middlewares/authMiddleware.js";
+import { sendMail } from "../utils/sendMail.js";
+
 
 export const signup = async(req, res) => {
   
@@ -17,12 +20,24 @@ export const signup = async(req, res) => {
   const newPrincipal = await prisma.principal.create({
     data:{
       name,
-      email,
+      email: email.trim().toLowerCase(),
       password:hashPassword,
+    },
+    select:{
+      name:true,
+      email:true
     }
   });
 
-  console.log(newPrincipal);
+   await sendMail({
+    to:newPrincipal.email,
+    subject: `Account created`,
+    html:`
+    <p><b>Hello ${newPrincipal.name}</b></p>
+    <p>Your account has been created successfully.Now you can login.</P>
+    `
+   }); 
+
   return res.status(201).json({ message: "principal created successfully", newPrincipal }); 
   } catch (error) {
     console.log("Error while signup", error);
@@ -40,14 +55,20 @@ export const login = async(req, res) => {
     if(!principal){
       return res.status(404).json({ message: "Principal not registered" });
     }
-
+     
     const checkPassword = await bcrypt.compare(password, principal.password);
 
     if(!checkPassword){
       return res.status(401).json({ message: "Invalid email or password" });
     }
+      const payLoad = {
+        id:principal.id,
+        email:principal.email,
+      }
 
-     return res.status(200).json({ message: "Principal login successfully" });
+      const token = generateToken(payLoad);
+
+     return res.status(200).json({ message: "Principal login successfully", token: token });
 
      } catch (error) {
       console.log("Error while login", error);
