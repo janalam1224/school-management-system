796b8c1c1768b5f56211930b7ef5e1d8b51b8b56
janalam <janalam1224@gmail.com>
2025-10-02 15:30:36 +0500

    Modify app.js

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import prisma from './config/db_config.js';
+
 import authRouter from './routes/authRouter.js';
 import principalRouter from './routes/principalRouter.js';
+import classRouter from './routes/classRouter.js';
+import teacherRouter from './routes/teacherRouter.js'
+import subjectRouter from './routes/subjectRouter.js';
+import studentRouter from './routes/studentRouter.js';
+import excelRouter from './routes/excelRouter.js';
+import invoiceRouter from './routes/invoiceRouter.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRouter);
 app.use("/api/principal", principalRouter);
+app.use("/api/classes", classRouter);
+app.use("/api/teachers", teacherRouter);
+app.use("/api/subjects", subjectRouter);
+app.use("/api/students", studentRouter);
+app.use("/api", excelRouter);
+app.use("/api", invoiceRouter);
 
-const PORT = 3100;
-
-app.listen(PORT, () => {
-  console.log(`Server is Listening on http://localhost:${PORT}`);
-});
+export default app;
