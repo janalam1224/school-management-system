bffdb9bb445b1af1d05e382623637f4865db5f08
janalam <janalam1224@gmail.com>
2025-10-03 09:40:17 +0500

    Modify assignmentController.js

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -1,25 +1,31 @@
 import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import prisma from "../config/db_config";
+import prisma from "../config/db_config.js";
 
-export const getTeacherClassSubjects = asyncHandler(async(req, res) => {
+export const getAssignment = asyncHandler(async(req, res) => {
 
 });
 
-export const createTeacherClassSubject = asyncHandler(async(req, res) => {
+export const createAssignment = asyncHandler(async(req, res) => {
   const { teacherId, classId, subjectId } = req.body;
-
-    const existing = await prisma.teacherSubjectClass.findUnique({
-      where:{ teacherId, subjectId, classId}
-    });
-      if(existing){
-       throw new apiError(409, "Already exists");
-      }
+   const existing = await prisma.teacherSubjectClass.findUnique({
+   where: {
+    teacherId_subjectId_classId: {
+      teacherId,
+      subjectId,
+      classId
+    }
+  }
+ });
       
-     const  newTeacherClassSubject = await prisma.teacherSubjectClass.create({
+     const  newAssignment = await prisma.teacherSubjectClass.create({
+           data:{
            teacherId,
            classId,
            subjectId,
+           }
        }); 
+
+       return res.status(201).json(new apiResponse(201, "Assign successfully"));
 });
