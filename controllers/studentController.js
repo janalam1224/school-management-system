6cace85b37f1461a3dc4d28a188b1aa66d7860d2
janalam <janalam1224@gmail.com>
2025-10-03 09:44:30 +0500

    Add studentController.js

---
+++ controllers/studentController.js
@@ -0,0 +1,22 @@
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const getStudents = asyncHandler(async(req, res) => {
+});
+
+export const createStudent = asyncHandler(async(req, res) => {
+ 
+});
+
+export const findStudent = asyncHandler(async(req, res) => {
+
+});
+
+export const editStudent = asyncHandler(async(req, res) => {
+
+});
+
+export const deleteStudent = asyncHandler(async(req, res) => {
+
+});
