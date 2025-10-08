9d97b24e5958e5cc043c3b43333e7afb9a9c3ff4
janalam <janalam1224@gmail.com>
2025-10-08 09:57:00 +0500

    Add resultController

---
+++ controllers/resultController.js
@@ -0,0 +1,12 @@
+import prisma from "../config/db_config.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const getResults = asyncHandler(async(req, res) => {
+  
+});
+
+export const createResult = asyncHandler(async(req, res) => {
+  
+});
