e6ca30e93dc4b5cd2dae0830cd7045158ec7d8d7
janalam <janalam1224@gmail.com>
2025-10-09 10:39:41 +0500

    Add examController

---
+++ controllers/examController.js
@@ -0,0 +1,12 @@
+import prisma from "../config/db_config.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const getExams = asyncHandler(async(req, res) => {
+  
+});
+
+export const createExam = asyncHandler(async(req, res) => {
+  
+});
